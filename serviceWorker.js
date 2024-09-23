5320c646229a554f1980f5ffaf32cfdb99fba215
Hassan Raza <mrismart70@gmail.com>
2024-09-23 20:51:35 +0500

    Created a serviceWorker.js file and added array of assets to be cached

---
+++ serviceWorker.js
@@ -0,0 +1,19 @@
+const staticToDoList = "to-do-list-site-v1";
+
+const assets = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/script.js",
+    "/images/icon.png",
+    "/images/unchecked.png",
+    "/images/checked.png"
+];
+
+self.addEventListener("install", installEvent => {
+   installEvent.waitUntil(
+    caches.open(staticToDoList).then(cache => {
+        cache.addAll(assets)
+    })
+   )
+});
