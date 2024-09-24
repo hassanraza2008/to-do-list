b36113e6856dde8afc1d737dc3705080c476d217
Hassan Raza <127892003+hassanraza2008@users.noreply.github.com>
2024-09-24 13:41:45 +0500

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,7 @@
+// Define the cache name for the static assets
 const staticToDoList = "to-do-list-site-v1";
 
+// List of assets to cache
 const assets = [
   "/",
   "/index.html",
@@ -10,18 +12,20 @@ const assets = [
   "/images/checked.png",
 ];
 
+// Install event - caching the static assets
 self.addEventListener("install", (installEvent) => {
   installEvent.waitUntil(
     caches.open(staticToDoList).then((cache) => {
-      cache.addAll(assets);
+      cache.addAll(assets); // Add all specified assets to the cache
     })
   );
 });
 
+// Fetch event - serving cached assets if available
 self.addEventListener("fetch", (fetchEvent) => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then((res) => {
-      return res || fetch(fetchEvent.request);
+      return res || fetch(fetchEvent.request); // Return cached asset or fetch from network if not in cache
     })
   );
 });
